a8923c651c069f6e273bb432b8368721b8b5753b
Michell Rezende de Paula e Silva <michell.rezende.p@gmail.com>
2024-04-27 14:02:56 -0300

    Exercício 4: go-division-numbers

---
+++ go-division-numbers/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(i1 int, i2 int) (int, error) {
+	if i2 == 0 {
+		return 0, errors.New("division by zero")
+	}
+
+	return (i1 / i2), nil
+}
+
+func main() {
+	l1 := [10]int{43, 67, 125, 96, 78, 104, 45, 99, 81, 55}
+	l2 := [10]int{11, 29, 52, 47, 30, 0, 18, 66, 2, 88}
+
+	for i := 0; i < 10; i++ {
+		q, e := divide(l1[i], l2[i])
+
+		if e != nil {
+			fmt.Println(e)
+			fmt.Printf("%d / %d \n", l1[i], l2[i])
+			continue
+		}
+
+		fmt.Printf("%d / %d = %d \n", l1[i], l2[i], q)
+	}
+
+	/*
+		references:
+		https://pkg.go.dev/fmt#Sprintf
+	*/
+}
---
+++ go-division-numbers/readme.md
@@ -0,0 +1,13 @@
+Exercício 4
+
+- Inicie um módulo em Go com seu github e go-division-numbers
+    - Exemplo: github.com/Uemerson/go-division-numbers
+- Crie um arquivo main.go
+- Crie duas listas com 10 elementos inteiros
+- Crie uma função que tenha dois parâmetros e que retorne a divisão do primeiro parâmetro com o segundo e um erro
+    - Observação: caso o segundo parâmetro for zero o erro deve ser diferente de nil
+- Percorra ambas listas e chame a função criada
+- O programa deve ter dez saídas diferentes. Exemplo:
+    - 10 / 2 = 5
+    - error: division by zero.
+    - ...
